e23cccce1839d8ffbb5ead0c098a7bba3e9cfca4
chw <chw_1118@outlook.com>
2022-07-22 06:34:07 +0800

    first commit

---
+++ src/main/java/com/chw/git/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.chw.git.test;
+
+/**
+ * @author chw1113
+ * @create 2022-07-22 6:29
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+
+}
